c0259c7933651bdd6af1cadc1998276bca58d7ce
isyerim3406 <isyerim3406@gmail.com>
2025-09-15 14:37:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: '/usr/bin/google-chrome-stable', // sistem Chrome kullan
+    executablePath: '/usr/bin/google-chrome-stable',
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const page = await browser.newPage();
-
-  // TradingView login ve cookie yükleme
   await page.goto('https://www.tradingview.com');
-  // Burada cookie yüklemesini yap
-  // await page.setCookie(...cookies);
-
-  console.log('Chrome başlatıldı, sayfa açıldı.');
-
-  // Örnek ekran yakalama (Buy/Sell pixel alanı)
-  const screenshot = await page.screenshot({ fullPage: true });
-  console.log('Screenshot alındı, byte length:', screenshot.length);
+  console.log('Chrome başlatıldı.');
 
   await browser.close();
 })();
